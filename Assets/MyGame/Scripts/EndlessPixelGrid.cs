56af1d37471bee21b69247514553876f6f67a0b6
David Dammerer <david.dammerer08@htl-salzburg.ac.at>
2026-01-30 13:37:09 +0100

    Add data model for grid and input line

--- Assets/MyGame/Scripts/EndlessPixelGrid.cs
+++ Assets/MyGame/Scripts/EndlessPixelGrid.cs
@@ -1,8 +1,36 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndlessPixelGrid : MonoBehaviour
 {
-    public bool[] inputLine = new bool[7];
-    public bool[,] grid = new bool[10, 7];
+    public Transform gridParent;    
+    public Transform inputParent;     
 
+    Image[,] gridImages = new Image[10, 7];
+    Image[] inputImages = new Image[7];
+
+    void Start()
+    {
+        AutoLinkUI();
+    }
+
+
+
+    void AutoLinkUI() // Automatisch ui elemente in array einistecken
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            inputImages[i] = inputParent.GetChild(i).GetComponent<Image>();
+        }
+
+        int index = 0;
+        for (int y = 0; y < 10; y++)
+        {
+            for (int x = 0; x < 7; x++)
+            {
+                gridImages[y, x] = gridParent.GetChild(index).GetComponent<Image>();
+                index++;
+            }
+        }
+    }
 }
